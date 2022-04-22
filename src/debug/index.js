9a86391dea6ee1c37410bfd5e53d1af27a87249c
Joaquin Grunwald <joaquingrunwald@gmail.com>
2022-04-21 21:35:09 -0300

    Añadida folder debug y archivo index.js

---
+++ src/debug/index.js
@@ -0,0 +1,12 @@
+var a = 'Hello'; //var global
+
+function hello(){
+  let b = 'Hello World';
+  const c = 'Hello World!';
+  if(true){ //true para que se cumpla ese if
+    let d = 'Hello World!!';
+    debugger
+  }
+}
+
+hello();
